53b4aaebb4a3f8d3d3a336e94fc53e06227df4a7
initdb <benedict.schwind@live.de>
2018-11-10 21:51:54 +0100

    make 02-remove.sh cleaner.

--- 06-shell:misc/02-scripts:remove.sh
+++ 06-shell:misc/02-scripts:remove.sh
@@ -4,10 +4,10 @@
 # 6.2 script which removes file* in while loop      #
 #####################################################
 
-i=0
-while [ $i -lt 100 ]
+i=1
+while [ $i -le 100 ]
     do
+        rm file$i
         i=$(expr $i + 1)
         # or let i+=1
-        rm file$i
     done
