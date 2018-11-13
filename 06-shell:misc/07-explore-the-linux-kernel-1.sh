293bcc507cb80ffa78af8f0447f41d16f2fceb28
initdb <benedict.schwind@live.de>
2018-11-13 17:52:44 +0100

    update comments

--- 06-shell:misc/07-explore-the-linux-kernel-1.sh
+++ 06-shell:misc/07-explore-the-linux-kernel-1.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 #####################################################
-# 6.7a print all unique file extensions into one    #
+# 6.7b print all unique file extensions into one    #
 # file                                              #
 #####################################################
 
 find . -type f -exec "../BS-specification/sheet_06_misc/getext.sh" -f {} \; | sort -u > tmp.txt
 
 #####################################################
-# 6.7b print number of c files in linux kernel      #
+# 6.7c print number of c files in the linux kernel  #
 #####################################################
 
 find . -type f -name "*.c" | wc -l
 
 #####################################################
-# 6.7c print number of lines in all c files in the  #
+# 6.7d print number of lines in all c files in the  #
 # linux kernel                                      #
 #####################################################
 
 find . -type f -name "*.c" -exec cat {} \; > count.txt; wc -l count.txt
 
 #####################################################
-# 6.7d rename all c files to cpp file in the        #
+# 6.7e rename all c files to cpp file in the        #
 # linux kernel                                      #
 #####################################################
 
--- 06-shell:misc/08-explore-the-linux-kernel-2.sh
+++ 06-shell:misc/08-explore-the-linux-kernel-2.sh
@@ -10,12 +10,23 @@ grep -r "fuck" . | wc -l
 # "-i" : ignores case
 grep -r -i "fuck" . | wc -l
 
-# also find fuck, pissed, bloddy, bastard within one pattern
-# egrep or grep -E needed
+#####################################################
+# 6.8d also find fuck, pissed, bloddy, bastard      #
+# within one pattern egrep or grep -E needed since  #
+# using regex.                                      #
+#####################################################
 egrep -r -i "(fuck|pissed|bloody|bastard)" . | wc -l
 
-# in which files ?
+#####################################################
+# 6.8f in which files are these hits(reverse order, # 
+# only uniques).                                    #
+#####################################################
 egrep -r -i "(fuck|pissed|bloody|bastard)" . | cut -d : -f 1 | sort -u -r
 
-# replace bloody with damned
+#####################################################
+# 6.8g replace bloody with damned in all files that #
+# contain bloody. cut -d : -f 1 only works with -f  #
+# xargs executes sed for each found file, wouldn't  #
+# work otherwise!!                                  #
+#####################################################
 grep -r -i -o "bloody" . | cut -d : -f 1 | xargs sed -i 's/bloody/damned/g'
