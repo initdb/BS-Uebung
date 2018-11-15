498de95a5f0f74c71c4d49f18e38d773a75e9311
initdb <benedict.schwind@live.de>
2018-11-15 21:24:13 +0100

    rename dirs + first to scripts

---
+++ 07-build_c_programs/02_simple_prog/02-build-with-a-makefile.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#####################################################
+# 7.3 build with a makefile                         #
+#####################################################
+# build and run simple_prog
+make && ./simple_prog
+
+# clean object files
+make clean
--- 07-build_c_programs/1_hello_world/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h> //EXIT_SUCCESS
-#include <stdio.h>  //printf
-
-int main(int argc, char const *argv[])
-{
-    printf("hello world\n");
-    return EXIT_SUCCESS;
-}
