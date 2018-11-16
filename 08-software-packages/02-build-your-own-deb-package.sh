c6b6f4005b506cccb96e0e02a29c4c88bc8e421c
initdb <benedict.schwind@live.de>
2018-11-16 22:38:35 +0100

    initial commit of exercise 9

---
+++ 08-software-packages/02-build-your-own-deb-package.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#####################################################
+# 8.2 build your own deb package                    #
+#####################################################
+
+test -d hello || mkdir hello
+
+cd hello
+
+# download file
+test -f hello-2.10.tar.gz || wget http://ftp.gnu.org/gnu/hello/hello-2.10.tar.gz
+
+# x: extract, z: unzip file
+tar xzf hello-2.10.tar.gz
+
+# create debian folder and files
+cd hello-2.10
+dh_make -f ../hello-2.10.tar.gz -s -y
+
+# delete unnecessary
+cd debian && rm *.ex *.EX README* 2> /dev/null
+
+test -f ../../changelog && cp ../../changelog .
+test -f ../../control && cp ../../control .
+test -f ../../copyright && cp ../../copyright .
+test -f ../../format && cp ../../format ../source/
+
+cd ..
+
+# configure the build
+./configure
+
+dpkg-buildpackage -us -uc
