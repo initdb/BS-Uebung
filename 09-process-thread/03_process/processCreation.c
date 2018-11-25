06fd09f5f5b63bd729aee339c072b4c278a26970
initdb <benedict.schwind@live.de>
2018-11-25 13:27:29 +0100

    create a single process

--- 09-process-thread/03_process/processCreation.c
+++ 09-process-thread/03_process/processCreation.c
@@ -7,10 +7,12 @@
 int counter = 0;
 
 void work() {
+    printf("doing heavy work... \n");
     sleep(20); //simulates the "heavy" work!!
 
     //TODO: Add code for created processes here
-    
+    counter++;
+    printf("done with %d\n", counter);
 }
 
 int main(int argc, char** argv){
@@ -26,7 +28,22 @@ int main(int argc, char** argv){
     }
 
     //TODO: Write your code here
-    pid_t pid[N];
+    //pid_t pid[N];
+
+    pid_t pid;
+    pid = fork();
+    if (pid < 0){
+        printf("fork failed!\n");
+        exit(1);
+    }
+    else if (pid == 0) {
+        printf("hi, I'm the child process\n");
+        work();
+        printf("goodbye\n");
+        exit(0);
+    } else {
+        printf("I'm the parent and I just forked a child, pid %d\n", pid);
+    }
 
     return EXIT_SUCCESS;
 }
