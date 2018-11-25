6569a064562ae677a61d175a0acb541cc34edad6
initdb <benedict.schwind@live.de>
2018-11-25 13:38:02 +0100

    impl. start of multiple processes. possible bug or intentinaly not possible?

--- 09-process-thread/03_process/processCreation.c
+++ 09-process-thread/03_process/processCreation.c
@@ -6,7 +6,8 @@
 
 int counter = 0;
 
-void work() {
+void work()
+{
     printf("doing heavy work... \n");
     sleep(20); //simulates the "heavy" work!!
 
@@ -15,34 +16,43 @@ void work() {
     printf("done with %d\n", counter);
 }
 
-int main(int argc, char** argv){
-
+int main(int argc, char** argv)
+{
     int N = 0;
 
     //Get the number of processes which should be created
-    if(argc == 2){
+    if(argc == 2)
+    {
         N = atoi(argv[1]);
-    } else {
+    } 
+    else
+    {
         printf("Usage: %s N\n", argv[0]);
         exit(1);
     }
 
-    //TODO: Write your code here
-    //pid_t pid[N];
+    pid_t pid[N];
 
-    pid_t pid;
-    pid = fork();
-    if (pid < 0){
-        printf("fork failed!\n");
-        exit(1);
-    }
-    else if (pid == 0) {
-        printf("hi, I'm the child process\n");
-        work();
-        printf("goodbye\n");
-        exit(0);
-    } else {
-        printf("I'm the parent and I just forked a child, pid %d\n", pid);
+    while(counter < N)
+    {
+        //pid_t pid;
+        pid[counter] = fork();
+        if (pid[counter] < 0)
+        {
+            printf("fork failed!\n");
+            exit(1);
+        }
+        else if (pid[counter] == 0)
+        {
+            printf("hi, I'm the child process\n");
+            work();
+            printf("goodbye\n");
+            exit(0);
+        }
+        else
+        {
+            printf("I'm the parent and I just forked a child, pid %d\n", pid[counter]);
+        }
     }
 
     return EXIT_SUCCESS;
