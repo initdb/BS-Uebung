97f71a8cbbffdeb9ad4b60753db2bb5467f3f833
initdb <benedict.schwind@live.de>
2018-11-25 15:53:48 +0100

    improve error handling of thread impl

--- 09-process-thread/04_thread/threadCreation.c
+++ 09-process-thread/04_thread/threadCreation.c
@@ -5,27 +5,60 @@
 
 int counter = 0;
 
-void* work() {
+void* work()
+{
+    printf("doing heavy work... \n");
     sleep(20); //simulates the "heavy" work!!
-
-    //TODO: Add code for created threads here
     
+    counter++;
 
     return NULL;
 }
 
-int main(int argc, char** argv){
+int main(int argc, char** argv)
+{
     int N = 0;
 
-    if(argc == 2){
+    if(argc == 2)
+    {
         N = atoi(argv[1]);
-    } else {
+    }
+    else
+    {
         printf("Usage: %s N \n", argv[0]);
         exit(1);
     }
 
-    //TODO: Add code for the main thread here
     pthread_t pth[N];
+    int thread_create_state;
+    int thread_exit_state;
+
+    for(int i = 0; i < N; i++)
+    {
+        // create threads, which executes work
+        thread_create_state = pthread_create(&pth[i], NULL, &work, NULL);
+
+        if(thread_create_state != 0)
+        {
+            fprintf(stderr, "failed creating thread.\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    for(int i = 0; i < N; i++)
+    {
+        // join thread, waits until the thread has finished
+        thread_exit_state = pthread_join(pth[i], NULL);
+
+        if(thread_exit_state != 0)
+        {
+            printf("thread exited with failure %d.\n", thread_exit_state);
+        }
+
+        printf("child thread exited with TID %lu exited.\n", pth[i]);
+    }
+
+    printf("counter: %d\n", counter);
 
     return EXIT_SUCCESS;
 }
