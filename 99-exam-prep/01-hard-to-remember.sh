a8a42e35a2372d4245259a464f4410f3a3545aef
initdb <benedict.schwind@live.de>
2019-01-12 21:35:54 +0100

    add shell script parameters and controll structures to cheat sheet

--- 99-exam-prep/01-hard-to-remember.sh
+++ 99-exam-prep/01-hard-to-remember.sh
@@ -60,3 +60,40 @@ test "string" != "string"
 # link multiple test commands
 test -r file -a -w file
 test -r file -o -w file
+
+#####################################################
+# shell: script parameter                           #
+#####################################################
+# $0    |   Name of the script
+# $1    |   First parameter of shell script (or function)
+# $2    |   Second parameter of shell script (or function)
+# ...
+# ${10} |   10th parameter of shell script (or function)
+# ${11} |   11th parameter of shell script (or function)
+# ...
+# $#    |   Number of parameters, starting from one
+# $*    |   Expands the parameters as one string, starting from one
+# $@    |   Expands each parameter in a separate string, starting from one   
+# $?    |   Exit status of the last command/process
+# $$    |   Process id of shell
+# $!    |   Process id of last command/process started asynchronously   
+
+#####################################################
+# shell: controll structures                        #
+#####################################################
+
+# create and fill files with index i
+for i in $(seq 1 1 9)
+do
+    echo "$i" > "file_$i"
+done
+
+# test: -a and -o or, but better to read is [] && [].
+# both ~ and $HOME possible
+# [[ && || ]] <-- supports normal c syntax! but not available on all systems ?!
+if [ $# -eq 1 ] && [ $1 = ~ ]; then
+    echo "Yes, this is your home directory."
+else
+    echo "No, this isn't your home directory."
+    exit 1
+fi
